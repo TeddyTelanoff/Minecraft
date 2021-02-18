934a527c85303564fb41ee9ad414600250ef2150
Treidex <treidexsemextetry@gmail.com>
2021-02-18 14:23:41 -0800

    chunk!

--- Assets/Scripts/World/Chunk.cs
+++ Assets/Scripts/World/Chunk.cs
@@ -3,51 +3,77 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    public static Vector3Int ChunkSize { get => new Vector3Int(3, 3, 3); }
-    public Vector2Int _position;
+	public static Vector3Int ChunkSize { get => new Vector3Int(16, 16, 16); }
+	public Vector2Int _position;
 
-    private Block[,,] _blocks = new Block[ChunkSize.x, ChunkSize.y, ChunkSize.z];
+	public Block[,,] _blocks = new Block[ChunkSize.x, ChunkSize.y, ChunkSize.z];
 
-    public void ReGenerateMesh() => GenerateMesh();
-    public void GenerateMesh()
-    {
-        var vertices = new List<Vector3>();
-        var triangles = new List<int>();
-        var uvs = new List<Vector2>();
+	public void ReGenerateMesh() => GenerateMesh();
+	public void GenerateMesh()
+	{
+		var vertices = new List<Vector3>();
+		var triangles = new List<int>();
+		var uvs = new List<Vector2>();
 
-        for (var x = 0; x < ChunkSize.x; x++)
-            for (var y = 0; y < ChunkSize.y; y++)
-                for (var z = 0; z < ChunkSize.z; z++)
-                {
-                    if (_blocks[x, y, z].BlockType == BlockType.Air)
-                        continue;
+		for (var x = 0; x < ChunkSize.x; x++)
+			for (var y = 0; y < ChunkSize.y; y++)
+				for (var z = 0; z < ChunkSize.z; z++)
+				{
+					if (_blocks[x, y, z].BlockType == BlockType.Air)
+						continue;
 
-                    // Front
-                    int firstVert = vertices.Count;
+					// Front
+					if (z <= 0 || _blocks[x, y, z - 1].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 0, y + 0, z + 0), Vector3.up, Vector3.right, false);
+					// Back
+					if (z >= ChunkSize.z - 1 || _blocks[x, y, z + 1].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 0, y + 0, z + 1), Vector3.up, Vector3.right, true);
 
-                    vertices.Add(new Vector3(x + 0, y + 0, z));
-                    vertices.Add(new Vector3(x + 1, y + 0, z));
-                    vertices.Add(new Vector3(x + 1, y + 1, z));
-                    vertices.Add(new Vector3(x + 0, y + 1, z));
+					// Right
+					if (x <= 0 || _blocks[x - 1, y, z].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 0, y + 0, z + 0), Vector3.up, Vector3.forward, true);
+					// Left
+					if (x >= ChunkSize.x - 1 || _blocks[x + 1, y, z].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 1, y + 0, z + 0), Vector3.up, Vector3.forward, false);
 
-                    triangles.Add(firstVert + 0);
-                    triangles.Add(firstVert + 1);
-                    triangles.Add(firstVert + 2);
+					// Top
+					if (y <= 0 || _blocks[x, y - 1, z].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 0, y + 0, z + 0), Vector3.forward, Vector3.right, true);
+					// Bottom
+					if (y >= ChunkSize.x - 1 || _blocks[x, y + 1, z].BlockType == BlockType.Air)
+						BuildMesh(ref vertices, ref triangles, ref uvs, new Vector3(x + 0, y + 1, z + 0), Vector3.forward, Vector3.right, false);
+				}
 
-                    triangles.Add(firstVert + 0);
-                    triangles.Add(firstVert + 2);
-                    triangles.Add(firstVert + 3);
-                }
+		var mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		mesh.RecalculateTangents();
 
-        var mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.uv = uvs.ToArray();
+		// Debug.Log($"Triangles[{triangles.Count}], Mesh Triangles[{mesh.triangles.Length}]");
 
-        mesh.RecalculateNormals();
+		GetComponent<MeshFilter>().sharedMesh = mesh;
+		GetComponent<MeshCollider>().sharedMesh = mesh;
+	}
 
-        GetComponent<MeshFilter>().mesh = null;
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshCollider>().sharedMesh = mesh;
-    }
+	private void BuildMesh(ref List<Vector3> vertices, ref List<int> triangles, ref List<Vector2> uvs, Vector3 pos, Vector3 up, Vector3 right, bool reverse)
+	{
+		int firstVert = vertices.Count;
+		int irev = reverse ? 1 : 0;
+
+		vertices.Add(pos + up);
+		vertices.Add(pos + up + right);
+		vertices.Add(pos + right);
+		vertices.Add(pos);
+
+		triangles.Add(firstVert + 0);
+		triangles.Add(firstVert + 1 + irev);
+		triangles.Add(firstVert + 2 - irev);
+
+		triangles.Add(firstVert + 0);
+		triangles.Add(firstVert + 2 + irev);
+		triangles.Add(firstVert + 3 - irev);
+	}
 }
--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
+	public GameObject _chunkPrefab;
+	public float _heightStep;
+
 	private Dictionary<Vector2Int, Chunk> _chunks = new Dictionary<Vector2Int, Chunk>();
+	private void Start() => GenerateChunk(new Vector2Int(0, 0));
 
-    private void Start() => GenerateChunk(new Vector2Int(0, 0));
+	public void GenerateChunk(Vector2Int pos)
+	{
+		Debug.Assert(!_chunks.ContainsKey(pos), $"Chunk Already Generated at [{pos.x}, {pos.y}]");
 
-    public void GenerateChunk(Vector2Int pos)
-    {
-        Debug.Assert(!_chunks.ContainsKey(pos), $"Chunk Already Generated at [{pos.x}, {pos.y}]");
+		var chunk = Instantiate(_chunkPrefab, new Vector3(pos.x * Chunk.ChunkSize.x, 0, pos.y * Chunk.ChunkSize.z), Quaternion.identity);
+		chunk.name = $"Chunk ({pos.x}, {pos.y})";
+		Chunk chunkComponent = chunk.GetComponent<Chunk>();
+		chunkComponent._position = pos;
 
-        var chunk = new GameObject($"Chunk ({pos.x}, {pos.y})");
-        chunk.transform.position = new Vector3(pos.x * Chunk.ChunkSize.x, 0, pos.y * Chunk.ChunkSize.z);
-        Chunk chunkComponent = chunk.AddComponent<Chunk>();
-        chunk.AddComponent<MeshFilter>();
-        chunk.AddComponent<MeshRenderer>();
-        chunk.AddComponent<MeshCollider>();
+		for (var x = 0; x < Chunk.ChunkSize.x; x++)
+			for (var z = 0; z < Chunk.ChunkSize.z; z++)
+			{
+				int height = Mathf.FloorToInt(Mathf.PerlinNoise((pos.x + x) * _heightStep, (pos.y + z) * _heightStep) * Chunk.ChunkSize.y);
+				for (var y = 0; y < height; y++)
+					chunkComponent._blocks[x, y, z].BlockType = BlockType.Grass;
+				//for (var y = 0; y < Chunk.ChunkSize.y; y++)
+				//	chunkComponent._blocks[x, y, z].BlockType = Random.value > 0.5 ? BlockType.Air : BlockType.Grass;
+			}
 
-        chunkComponent._position = pos;
-        chunkComponent.GenerateMesh();
-    }
+		chunkComponent.GenerateMesh();
+	}
 }
